b2d762993ac641bc490024e5d4b5c4a5bef649da
muralov <m_uralov@yahoo.com>
2016-05-11 09:43:38 +0200

    Get and Delete has been added. In addition, JSON input object transformation obtained successfully.

--- family_planner/modules/meals/src/main/java/home/family_planner/meals/rest/FoodProductController.java
+++ family_planner/modules/meals/src/main/java/home/family_planner/meals/rest/FoodProductController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import home.family_planner.meals.models.FoodProduct;
@@ -25,24 +28,52 @@ public class FoodProductController {
 
     @Transactional
 	@RequestMapping(method=RequestMethod.POST)
-	@ResponseBody
-    public FoodProduct add(FoodProduct foodProduct) {
-    	foodProduct = new FoodProduct("Carrot",  "Carrot is vegetable and has a yellow color.", 1.00);
+    public @ResponseBody FoodProduct add(@RequestBody FoodProduct input) {
+		foodProductRepository.save(input);
+        return input;
+    }
+    
+    @Transactional
+	@RequestMapping(value="/{id}", method=RequestMethod.POST)
+    public 	@ResponseBody FoodProduct update(@PathVariable("id") Long id, @RequestBody FoodProduct input) {
+    	validateFoodProduct(id);
+    	FoodProduct foodProduct = foodProductRepository.findOne(id);
+    	foodProduct.setTitle(input.getTitle());
+    	foodProduct.setDescription(input.getDescription());
+    	foodProduct.setPrice(input.getPrice());
 		foodProductRepository.save(foodProduct);
         return foodProduct;
     }
+    
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public @ResponseBody FoodProduct get(@PathVariable("id") Long id) {
+		validateFoodProduct(id);
+        return foodProductRepository.findOne(id);
+    }
 	
 	@RequestMapping(method=RequestMethod.GET)
-	@ResponseBody
-    public List<FoodProduct> foods() {
+    public @ResponseBody List<FoodProduct> foods() {
         return foodProductRepository.findAll();
     }
 	
 	@Transactional
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-	@ResponseBody
     public void delete(@PathVariable("id") Long id) {
+		validateFoodProduct(id);
         foodProductRepository.delete(id);
     }
+	
+	private void validateFoodProduct(Long id) {
+		this.foodProductRepository.findOne(id);
+	}
 
+}
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class FoodProductNotFoundException  extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public FoodProductNotFoundException(Long id) {
+		super("could not find food product '" + id + "'.");
+	}
 }
