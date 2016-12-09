57a737f29903ef1d3e71eb05b32e84dffa500b24
Mansur Ubaydullo <mansur@linux-94w7.site>
2016-12-09 12:42:35 +0100

    Meals list has been developed with component of latest AngularJS.

---
+++ family_planner/web/meals-ui/app/meals/mealList.component.js
@@ -0,0 +1,24 @@
+/**
+ * Created by mansur on 12/8/16.
+ */
+'use strict';
+
+var meals = angular.module('meals');
+
+meals.component('mealList', {
+    templateUrl: 'meals/groceryList.html',
+    controller: function MealListCtrl(GroceryService) {
+        var self = this;
+
+        GroceryService.getFoodProductsPromise().then(
+            function (result) {
+                self.groceryItems = result;
+            }
+        );
+
+        this.removeItem = function (entry) {
+            GroceryService.removeItem(entry);
+        }
+
+    }
+});
---
+++ family_planner/web/meals-ui/app/meals/meals.module.js
@@ -0,0 +1,6 @@
+/**
+ * Created by mansur on 12/8/16.
+ */
+'use strict';
+
+angular.module('meals', []);
