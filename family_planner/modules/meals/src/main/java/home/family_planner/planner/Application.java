54640ffb01a8471ee10d0c6c518a192968807172
Mansur Uralov <m_uralov@yahoo.com>
2017-12-11 22:31:45 +0100

    New swagger version has been used for REST API documentation.

--- family_planner/modules/meals/src/main/java/home/family_planner/planner/Application.java
+++ family_planner/modules/meals/src/main/java/home/family_planner/planner/Application.java
@@ -4,45 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@EnableSwagger2
 @SpringBootApplication
 @ComponentScan
 public class Application {
 	
-//	private static final Logger log = LoggerFactory.getLogger(Application.class);
-	
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-	
-//	@Autowired 
-//	FoodProductRepository repository;
-//	
-//	@Bean
-//	public CommandLineRunner demo() {
-//		return (args) -> {
-//			// save a couple of customers
-//			repository.save(new FoodProduct("Carrot",  "Carrot is vegetable and has a yellow color.", 1.00));
-//			repository.save(new FoodProduct("Potatoe",  "Potatoe is vegetable and has a light brown color.", 0.75));
-//			repository.save(new FoodProduct("Bread",  "Bread should be cooked. It contains flour, water, etc.", 0.80));
-//
-//			// fetch all customers
-//			log.info("Customers found with findAll():");
-//			log.info("-------------------------------");
-//			for (FoodProduct customer : repository.findAll()) {
-//				log.info(customer.toString());
-//			}
-//            log.info("");
-//
-//			// fetch an individual customer by ID
-//			FoodProduct customer = repository.findOne(1L);
-//			log.info("Customer found with findOne(1L):");
-//			log.info("--------------------------------");
-//			log.info(customer.toString());
-//            log.info("");
-//		};
-//	}
 
 }
--- family_planner/modules/planner/src/main/java/home/family_planner/planner/Application.java
+++ family_planner/modules/planner/src/main/java/home/family_planner/planner/Application.java
@@ -1,15 +1,9 @@
 package home.family_planner.planner;
 
-import com.mongodb.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.net.UnknownHostException;
-
-@EnableSwagger2
 @SpringBootApplication
 @ComponentScan
 public class Application {
