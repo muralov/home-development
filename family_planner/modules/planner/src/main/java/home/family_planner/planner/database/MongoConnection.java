dff4aad3d813a16588e9766d1fb96437b53b14f6
muralov <m_uralov@yahoo.com>
2018-05-11 01:39:44 +0200

    Configuration parameters added for planner service DB connection

--- family_planner/modules/planner/src/main/java/home/family_planner/planner/database/MongoConnection.java
+++ family_planner/modules/planner/src/main/java/home/family_planner/planner/database/MongoConnection.java
@@ -1,6 +1,7 @@
 package home.family_planner.planner.database;
 
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -11,12 +12,21 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @Configuration
 public class MongoConnection {
 
+    @Value("${mongodb.hostname}")
+    private String hostname;
+
+    @Value("${mongodb.port}")
+    private int port;
+
+    @Value("${mongodb.database}")
+    private String database;
+
     public @Bean MongoClient mongoClient() {
-        return new MongoClient("planner-db");
+        return new MongoClient(hostname, port);
     }
 
     public @Bean
     MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "planner");
+        return new MongoTemplate(mongoClient(), database);
     }
 }
